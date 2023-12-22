a7d78dcb533e6525c76cd42b1fbba09425f8bd1b
trutleEcho <129397457+trutleEcho@users.noreply.github.com>
2023-12-22 17:15:56 +0530

    1

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+export default function Layout(){
+    return(
+        <>
+        <header className='border-solid border-2 border-black text-3xl font-semibold p-3 bg-green-600/90 text-white '>
+        SHIVAM SALES CORPORATIONS
+      </header>
+      <nav className='text-xl top-0 border-dashed p-0.5 border-black border-4 sticky bg-lime-400/50'>
+            navbar
+      </nav>
+      <div className="h-screen bg-cl-#C1F2B0">
+      </div>
+        </>
+      
+    )
+}
