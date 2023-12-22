4003097161bfafdc8ea3d1ff3190acd0d8e28a2e
trutleEcho <129397457+trutleEcho@users.noreply.github.com>
2023-12-22 19:07:54 +0530

    save1

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,44 @@
 import Image from 'next/image'
-import styles from './page.module.css'
+import styles from '../styles/page.module.css'
 import Layout from '@/components/Header'
+import { Container } from 'postcss'
 export default function Home() {
   return (
     <>
     <Layout></Layout>
-    
+    <section className={styles.con}>
+      <div style={{height:'300px',width:'300px',border:'solid'}}>
+       <Image src="next.svg" height={300} width={300}/>
+      </div>
+      <div>
+        hello
+      </div>
+    </section>
+
+
+    <section id='brand-info'className={styles.brandinfo}>
+      <div id='brand-image' style={{border:'solid'}} >
+        <Image src="vercel.svg" height={300} width={300}/>
+      </div>
+      <div id='brand-data' className={styles.branddata} >
+        <p>
+          This is some info about the brand.This is some info about the brand.
+          This is some info about the brand.This is some info about the brand.
+          This is some info about the brand.This is some info about the brand.
+          This is some info about the brand.This is some info about the brand.
+          This is some info about the brand.This is some info about the brand.
+          This is some info about the brand.This is some info about the brand.
+          This is some info about the brand.This is some info about the brand.
+          This is some info about the brand.This is some info about the brand.
+          This is some info about the brand.This is some info about the brand.
+          This is some info about the brand.This is some info about the brand.
+          This is some info about the brand.This is some info about the brand.
+          This is some info about the brand.This is some info about the brand.
+        </p>
+      </div>
+    </section>
+
+
     </>
   )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,17 @@
+import Paper from '@mui/material/Paper';
+
 export default function Layout(){
     return(
         <>
-        <header className='border-solid border-2 border-black text-3xl font-semibold p-3 bg-green-600/90 text-white '>
+        
+        <header className='  text-3xl font-semibold p-3 bg-green-600/90 text-white '>
         SHIVAM SALES CORPORATIONS
       </header>
-      <nav className='text-xl top-0 border-dashed p-0.5 border-black border-4 sticky bg-lime-400/50'>
+      <nav className='text-xl top-0  p-0.5  sticky bg-lime-400/50'>
             navbar
       </nav>
-      <div className="h-screen bg-cl-#C1F2B0">
-      </div>
+      
+      
         </>
       
     )
